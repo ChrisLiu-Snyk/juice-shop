82ae3f112ac7224e34e712781c9d2d7955d88084
ChrisLiu-Snyk <xian.liu@snyk.io>
2025-06-19 16:38:55 +0800

    Update app.js
    adding random code vulnerabilities

--- app.js
+++ app.js
@@ -7,3 +7,10 @@ require('./lib/startup/validateDependencies')().then(() => {
   const server = require('./server')
   server.start()
 })
+
+async function getUserUnsafe1(req, res) {
+  const userId = req.params.id; // User-supplied ID from URL
+  // DANGEROUS: Direct string concatenation
+  const query = `SELECT * FROM users WHERE id = ${userId}`;
+  try {
+    const user = await db.query(query); // Execute raw query
